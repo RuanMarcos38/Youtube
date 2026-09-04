507cb2eb835e5d04df577ab99a14beb72ce94a17
RuanMarcos38 <ruanessencia8@gmail.com>
2026-09-04 16:47:51 -0300

    Exibir opção de exclusão na tela de processamentos

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,11 @@
+import FailedProcessingActions from "@/components/FailedProcessingActions";
 import SaasApp from "@/components/SaasApp";
 
 export default function Home() {
-  return <SaasApp />;
+  return (
+    <>
+      <SaasApp />
+      <FailedProcessingActions />
+    </>
+  );
 }
